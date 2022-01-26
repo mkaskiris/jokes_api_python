ef499c27b6a18033eb969ce520ffc4b088e3a582
Menelaos <mkaskiris@hotmail.com>
2022-01-26 11:35:53 +0000

    added email service

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
 from flask import Flask, jsonify, request
-from flask_mail import Mail, Message
 from flask_cors import CORS
 from controllers import jokes
 from werkzeug import exceptions
+import smtplib
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'dustmastergeneral@gmail.com'
-app.config['MAIL_PASSWORD'] = 'dustypete'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
 
-# app.config['MAIL_SERVER']='smtp.mailtrap.io'
-# app.config['MAIL_PORT'] = 2525
-# app.config['MAIL_USERNAME'] = '09c179eaa9d3ba'
-# app.config['MAIL_PASSWORD'] = '831aa882d2acdc'
-# app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USE_SSL'] = False
+def email_setup(resp):
 
-mail = Mail(app)
+    gmail_user = 'kaskas1334@gmail.com'
+    gmail_password = 'vgnnhnlrsqdlhqxl'
+    recipient = 'mkaskiris@hotmail.com'
+    email_text = resp['joke'] + '\t' + resp['punchline']
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(gmail_user,
+                        recipient,
+                        email_text)
+        server.close()
+    except Exception as e:
+        print(f'Something went wrong, {e}')
+
 
 @app.route('/')
 def home():
     return jsonify({'message': 'Hello from Team J.U.M.P!'}), 200
+
 
 @app.route('/api/jokes', methods=['GET', 'POST'])
 def jokes_handler():
@@ -36,35 +41,32 @@ def jokes_handler():
     resp, code = fns[request.method](request)
     return jsonify(resp), code
 
-@app.route('/api/jokes/<int:id>', methods=['GET','POST'])
+
+@app.route('/api/jokes/<int:id>', methods=['GET', 'POST'])
 def joke_handler(id):
     fns = {
         'GET': jokes.show,
         'POST': jokes.create
     }
     resp, code = fns[request.method](request, id)
-    # print(resp)
-    # print(resp["joke"] + resp["punchline"])
-    # msg = Message('ok', 
-    #             sender='dustmastergeneral@gmail.com', 
-    #             recipients = ['mkaskiris@hotmail.com'])
-    # msg.body('Hello there')
-    # #msg.body(resp["joke"] + resp["punchline"])
-    # mail.send(msg)
-
+    email_setup(resp)
     return jsonify(resp), code
+
 
 @app.errorhandler(exceptions.NotFound)
 def handle_404(err):
     return {'message': f'Oops! {err}'}, 404
 
+
 @app.errorhandler(exceptions.BadRequest)
 def handle_400(err):
     return {'message': f'Oops! {err}'}, 400
 
+
 @app.errorhandler(exceptions.InternalServerError)
 def handle_500(err):
     return {'message': f"It's not you, it's us"}, 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
